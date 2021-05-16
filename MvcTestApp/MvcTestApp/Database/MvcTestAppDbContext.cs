1feca1cb7c12fe32b950a2f58dd35d7d7c1df8d2
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2021-05-16 09:34:38 +0300

    Initial setup

---
+++ MvcTestApp/MvcTestApp/Database/MvcTestAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTestApp.Database
+{
+    public class MvcTestAppDbContext : DbContext
+    {
+        public MvcTestAppDbContext(DbContextOptions<MvcTestAppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContactRequest> ContactRequests { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ MvcTestApp/MvcTestApp/Migrations/20210516063346_Initial.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcTestApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactRequests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstBookAmount = table.Column<int>(type: "int", nullable: false),
+                    SecondBookAmount = table.Column<int>(type: "int", nullable: false),
+                    ThirdBookAmount = table.Column<int>(type: "int", nullable: false),
+                    Priority = table.Column<bool>(type: "bit", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactRequests");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
